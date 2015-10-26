f1c6b9fab1a201412ffd7360586a1c607305c863
vmartinezf <vanessa_1895@msn.com>
2015-10-26 10:43:15 +0100

    datos por pantalla client

--- client.py
+++ client.py
@@ -5,15 +5,20 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2])
+
+if PORT < 1024:
+    sys.exit("ERROR: PORT IS inCORRECT")
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE_LISTA = sys.argv[3:]
+LINE = ' '.join(LINE_LISTA)
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
